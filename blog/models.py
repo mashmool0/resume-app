a57e899c0808310d3c821952d9ced382ae3fa6cd
[Amir Mashayekhi] <[ap4games2017@gmail.com]>
2024-03-07 14:53:24 +0330

    Finish Blog App Create models and fake Database

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+import jdatetime
+
 
 # Create your models here.
+list_month = ['', 'فروردین', 'اردیبهشت', 'خرداد', 'تیر', 'مرداد',
+              'شهریور', 'مهر', 'آبان', 'آذر', 'دی', 'بهمن', 'اسفند']
+
+month = list_month[int(str(jdatetime.date.today()).split('-')[1])]
+day = str(jdatetime.date.today()).split('-')[2]
+
+class Blog_post(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+    cover = models.ImageField(default='False')
+    month_def = models.CharField(max_length=20,default=month) 
+    day_def = models.CharField(max_length=20,default=day)
+
+    
+    def __str__(self) -> str:
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from about.models import HeaderInfo 
+from .models import Blog_post
 # Create your views here.
 
 
 def blog(request) : 
+    blog = Blog_post.objects.all() 
     info = HeaderInfo.objects.last() 
-    return render(request,'blog.html',context={'info':info,})
+    return render(request,'blog.html',context={'info':info,'blog':blog})
