51ecd05a38ec40c6a20638fbe3a96ff0b47b1988
[Amir Mashayekhi] <[ap4games2017@gmail.com]>
2024-03-07 12:39:44 +0330

    Set Model For Works and Crate Database for him

--- work/models.py
+++ work/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Sample(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+    image = models.ImageField()
+    githublink = models.URLField(null=False,default='#')
+    
+    
+    def __str__(self) -> str:
+        return self.title
--- work/views.py
+++ work/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from about.models import HeaderInfo
+from .models import Sample 
 # Create your views here.
 
 
 def work(request):
+    sample = Sample.objects.all()
     info = HeaderInfo.objects.last()
-    return render(request, 'works.html', context={'info': info})
+    return render(request, 'works.html', context={'info': info,'sample':sample})
+
