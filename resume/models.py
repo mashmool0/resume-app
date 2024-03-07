943ccc1d76d1acfd77568a35c69a09105ed403ed
[Amir Mashayekhi] <[ap4games2017@gmail.com]>
2024-03-07 11:42:00 +0330

    STR Return for models in resume

--- resume/models.py
+++ resume/models.py
@@ -7,14 +7,23 @@ class Education(models.Model):
     UniName = models.CharField(max_length=40)
     year = models.CharField(max_length=20)
     description = models.CharField(max_length=100)
+    def __str__(self) -> str:
+        return self.UniName 
 
 
 class Experience(models.Model):
     title = models.CharField(max_length=40)
     year = models.CharField(max_length=20)
     description = models.CharField(max_length=100)
+    def __str__(self) -> str:
+        return self.title
 
 
 class Skills(models.Model):
     title = models.CharField(max_length=40)
     level = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.title 
+    
+    
