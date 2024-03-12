f9cab389fb6d9ce624b224166df28d1f48c0e03a
[Amir Mashayekhi] <[ap4games2017@gmail.com]>
2024-03-12 21:00:44 +0330

    fix github link

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+import jdatetime
 # Create your models here.
+
+
+class Mail(models.Model):
+    FullName = models.CharField(max_length=50)
+    email = models.CharField(max_length=200)
+    text = models.TextField()
+    date = models.CharField(default=str(jdatetime.date.today()),max_length=20,null=True)
+
+    def __str__(self) -> str:
+        return self.email
--- contact/views.py
+++ contact/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
-from about.models import HeaderInfo 
+from about.models import HeaderInfo
+from .models import Mail
+
 # Create your views here.
 
 
 def contact(request):
+    if (request.method == "POST"):
+        nameContact = request.POST.get('nameContact')
+        emailContact = request.POST.get('emailContact')
+        messageContact = request.POST.get('messageContact')
+        if nameContact and emailContact and messageContact:
+            Mail.objects.create(FullName=nameContact,
+                                email=emailContact, text=messageContact)
+            
+            
+    
     info = HeaderInfo.objects.last()
-    return render(request,'contact.html',context={'info':info,})
+    return render(request, 'contact.html', context={'info': info, })
